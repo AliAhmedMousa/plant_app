d77c17f6ff41d5d9d661ff702778cc9dd9833305
AliAhmedMousa <aliahmedmousa@gmail.com>
2023-01-07 08:25:45 +0200

    .

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'plent',
+      title: 'Plant App',
       theme: ThemeData(
         scaffoldBackgroundColor: kBackgroundColor,
         primaryColor: kPrimaryColor,
